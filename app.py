4789934f9048c07bfb0d7d121db104e211020573
Shiv Akash <shivakash2003@gmail.com>
2024-03-20 20:04:24 +0530

    fix: cors

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from services import postSeller as ps
 from services import postBuyer as pb
+from services import postPost as pp
 
 app = Flask(__name__)
 CORS(app)
@@ -27,6 +28,23 @@ def post_buyer():
     buyer_address = req.get('buyer_address')
     buyer_contact_number = req.get('buyer_contact_number')
     return pb.postUser(buyer_name, buyer_address, buyer_contact_number)
+
+@app.route('/api/postPost', methods=['POST'])
+def create_post_route():
+    req = request.get_json()
+    restaurant_id = req.get('restaurant_id')
+    food_name = req.get('food_name')
+    item_quantity = req.get('item_quantity')
+    claimer = req.get('claimer')
+    status = req.get('status')
+    return pp.postPost(restaurant_id, food_name, item_quantity, claimer, status)
+
+@app.after_request
+def apply_cors(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+    return response
 
 # # main driver function
 if __name__ == '__main__':
